a3439836b92d2e2749a32470cff803048b8ce005
Mark Ericksen <brainlid@gmail.com>
2019-08-05 08:23:58 -0600

    added initial customer struct

---
+++ lib/code_flow/customer.ex
@@ -0,0 +1,18 @@
+defmodule CodeFlow.Customer do
+  @moduledoc """
+  Customer struct used in Code Flow examples.
+  """
+  alias __MODULE__
+
+  @enforce_keys [:name]
+  defstruct [:name, :location, :contact_name, :contact_number, {:orders, []}, {:active, true}]
+
+  @type t :: %Customer{
+          name: String.t(),
+          location: nil | String.t(),
+          contact_name: nil | String.t(),
+          contact_number: nil | String.t(),
+          orders: list(),
+          active: boolean()
+        }
+end
