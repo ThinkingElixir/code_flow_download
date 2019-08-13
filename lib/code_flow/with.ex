83709830412562344aac242f5af6118653c06927
Mark Ericksen <brainlid@gmail.com>
2019-08-13 05:17:14 -0600

    fixing master with

--- lib/code_flow/with.ex
+++ lib/code_flow/with.ex
@@ -2,12 +2,13 @@ defmodule CodeFlow.With do
   @moduledoc """
   Defining a workflow or "Code Flow" using the Railway Pattern.
   """
+  alias CodeFlow.Schemas.Order
   # alias CodeFlow.Fake.Customers
   # alias CodeFlow.Fake.Orders
   # alias CodeFlow.Fake.Items
 
   @spec place_new_order(customer_id :: integer, item_id :: integer, quantity :: integer) ::
-          {:ok, CodeFlow.Schemas.Order.t()} | {:error, String.t()}
+          {:ok, Order.t()} | {:error, String.t()}
   def place_new_order(customer_id, item_id, quantity) do
 
   end
