807a760a6cad69cf1472574a40edf3bdcac76a1d
Mark Ericksen <brainlid@gmail.com>
2019-09-01 09:46:20 -0600

    working on recursion solutions

--- lib/code_flow/recursion.ex
+++ lib/code_flow/recursion.ex
@@ -1,4 +1,6 @@
 defmodule CodeFlow.Recursion do
+  alias CodeFlow.Fake.Customers
+
   @moduledoc """
   Fix or complete the code to make the tests pass.
   """
@@ -7,14 +9,32 @@ defmodule CodeFlow.Recursion do
   Sum a list of numbers using recursion.
   """
   def sum_numbers(_numbers) do
-
   end
 
   @doc """
   Sum a list of purchases. A "purchase" entry is a map.
   """
   def sum_purchases(_customers) do
+  end
 
+  def create_customers(number) do
+    do_create_customers(number, 0)
+  end
+
+  defp do_create_customers(total, num) when num <= total do
+    case Customers.create(%{name: "Customer #{num}"}) do
+      {:ok, _customer} ->
+        :ok
+
+      {:error, _reason} ->
+      # TODO: Don't raise!? Keep a good API
+        raise "Failed trying to create customer!"
+    end
+    do_create_customers(total, num + 1)
+  end
+
+  defp do_create_customers(total, _num) do
+    "Created #{total} customers!"
   end
 
   @doc """
@@ -24,7 +44,10 @@ defmodule CodeFlow.Recursion do
 
   https://en.wikipedia.org/wiki/Fibonacci_number
   """
-  def fibonacci(_num) do
+  def fibonacci(0), do: 0
+  def fibonacci(1), do: 1
 
+  def fibonacci(index) do
+    fibonacci(index - 2) + fibonacci(index - 1)
   end
 end
