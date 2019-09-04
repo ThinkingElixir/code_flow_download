4ad682fc32faaa5726541a888d714f65ea163514
Mark Ericksen <brainlid@gmail.com>
2019-09-04 07:33:58 -0600

    added master enum code

--- lib/enum_shortcut.ex
+++ lib/enum_shortcut.ex
@@ -3,13 +3,20 @@ defmodule CodeFlow.EnumShortcut do
   Fix or complete the code to make the tests pass.
   """
   # alias CodeFlow.Fake.Customers
-
+  # alias CodeFlow.Schemas.OrderItem
 
   @doc """
   Create the desired number of customers. Provide the number of customers to
   create. Something like this could be used in a testing setup.
   """
   def create_customers(_number) do
+
+  end
+
+  @doc """
+  Sum a list of OrderItems to compute the order total.
+  """
+  def order_total(_order_items) do
 
   end
 end
