c8d2184751c989898cfa65ac384022fb89f32c9c
Mark Ericksen <brainlid@gmail.com>
2019-09-04 07:33:03 -0600

    added solutions

--- lib/enum_shortcut.ex
+++ lib/enum_shortcut.ex
@@ -2,8 +2,8 @@ defmodule CodeFlow.EnumShortcut do
   @moduledoc """
   Fix or complete the code to make the tests pass.
   """
-      alias CodeFlow.Fake.Customers
-
+  alias CodeFlow.Fake.Customers
+  alias CodeFlow.Schemas.OrderItem
 
   @doc """
   Create the desired number of customers. Provide the number of customers to
@@ -17,4 +17,21 @@ defmodule CodeFlow.EnumShortcut do
       {:ok, _customer} = Customers.create(%{name: "Customer #{num}"})
     end)
   end
+
+  # @doc """
+  # Sum a list of OrderItems to compute the order total.
+  # """
+  # def order_total(_order_items) do
+
+  # end
+
+  @doc """
+  Sum a list of OrderItems to compute the order total.
+  """
+  def order_total(order_items) do
+    Enum.reduce(order_items, 0, fn(%OrderItem{} = order_item, total) ->
+      (order_item.quantity * order_item.item.price) + total
+    end)
+  end
+
 end
