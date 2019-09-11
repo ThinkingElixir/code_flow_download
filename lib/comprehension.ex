2a1e4aa15f60293520e253f5fc2cf33b5b8bd0c3
Mark Ericksen <brainlid@gmail.com>
2019-09-11 08:27:21 -0600

    comprehension master file

---
+++ lib/comprehension.ex
@@ -0,0 +1,18 @@
+defmodule CodeFlow.Comprehension do
+  @moduledoc """
+  Fix or complete the code to make the tests pass.
+  """
+  alias CodeFlow.Schemas.User
+
+  def award_unfair_points(_users, _points) do
+
+  end
+
+  def build_chessboard() do
+
+  end
+
+  def team_points(_users) do
+
+  end
+end
