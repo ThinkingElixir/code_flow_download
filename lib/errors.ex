e1ecb497da7433540cf33a8bfa51b2810df4a2d5
Mark Ericksen <brainlid@gmail.com>
2019-09-14 13:56:13 -0600

    add errors master file for exercises

---
+++ lib/errors.ex
@@ -0,0 +1,15 @@
+defmodule CodeFlow.Errors do
+  @moduledoc """
+  Fix or complete the code to make the tests pass.
+  """
+  # alias CodeFlow.Fake.Orders
+  # alias CodeFlow.Schemas.Order
+
+  def find_user(_id) do
+
+  end
+
+  def find_order!(_id) do
+
+  end
+end
