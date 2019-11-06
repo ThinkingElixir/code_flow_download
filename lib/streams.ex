427dc8b88aab655494eda19eeea1356a1c0da78a
Mark Ericksen <brainlid@gmail.com>
2019-11-06 08:25:31 -0700

    experiments with stream

--- lib/streams.ex
+++ lib/streams.ex
@@ -31,14 +31,23 @@ defmodule CodeFlow.Streams do
   end
 
   def steps_in_enum3(data) do
-    self() |> Process.info() |> Keyword.get(:heap_size) |> IO.inspect(label: "Process heap size BEFORE")
+    self()
+    |> Process.info()
+    |> Keyword.get(:heap_size)
+    |> IO.inspect(label: "Process heap size BEFORE")
+
     data
     |> Enum.map(&(&1 * 2))
     |> Enum.map(&(&1 + 3))
-    |> Enum.map(&(to_string(&1)))
-    |> Enum.map(&("$#{&1}.00"))
+    |> Enum.map(&to_string(&1))
+    |> Enum.map(&"$#{&1}.00")
     |> Enum.take(5)
-    self() |> Process.info() |> Keyword.get(:heap_size) |> IO.inspect(label: "Process heap size AFTER")
+
+    self()
+    |> Process.info()
+    |> Keyword.get(:heap_size)
+    |> IO.inspect(label: "Process heap size AFTER")
+
     :ok
   end
 
@@ -76,24 +85,81 @@ defmodule CodeFlow.Streams do
   end
 
   def steps_in_stream3(data) do
-    self() |> Process.info() |> Keyword.get(:heap_size) |> IO.inspect(label: "Process heap size BEFORE")
+    self()
+    |> Process.info()
+    |> Keyword.get(:heap_size)
+    |> IO.inspect(label: "Process heap size BEFORE")
+
     data
     |> Stream.map(&(&1 * 2))
     |> Stream.map(&(&1 + 3))
-    |> Stream.map(&(to_string(&1)))
-    |> Stream.map(&("$#{&1}.00"))
+    |> Stream.map(&to_string(&1))
+    |> Stream.map(&"$#{&1}.00")
     # |> Enum.take(5)
     |> Enum.to_list()
-    self() |> Process.info() |> Keyword.get(:heap_size) |> IO.inspect(label: "Process heap size AFTER")
+
+    self()
+    |> Process.info()
+    |> Keyword.get(:heap_size)
+    |> IO.inspect(label: "Process heap size AFTER")
+
     :ok
   end
 
-# CodeFlow.Streams.steps_in_enum2([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-# CodeFlow.Streams.steps_in_stream2([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-# TODO: create a graphic that explains/shows the Enum process and one for the Stream process
-# The Stream doesn't execute a single call until something requests a value. Enum functions will do that.
+  @doc """
+  Count the number of words in a large text file.
+  """
+  @spec count_words(filename :: String.t()) :: {:ok, integer()} | {:error, String.t()}
+  def count_words(filename) do
+    File.open(filename, [:read], fn file ->
+      IO.read(file, :line)
+    end)
+  end
+
+  def via_resource(filename) do
+    file_stream =
+      Stream.resource(
+        fn -> File.open!(filename) end,
+        fn file ->
+          case IO.read(file, :line) do
+            data when is_binary(data) ->
+              word_count = data |> String.split(" ") |> Enum.count()
+              {[word_count], file}
+
+            _ ->
+              # :eof
+              {:halt, file}
+          end
+        end,
+        fn file -> File.close(file) end
+      )
+
+    Enum.to_list(file_stream)
+    # Returns a list of the number of words per line
+    #
+    # [33, 36, 33, 42, 21, 16, 12, 20, 19, 16, 10, 33, 36, 32, 38, 36, 39, 38, 15, 30,
+    #  18, 26, 22, 21, 40, 12, 27, 19, 15, 30, 21, 26, 37, 38, 29, 21, 15, 21, 25, 23,
+    #  21, 40, 28, 26, 44, 15, 22, 34, 18, 34, ...]
+  end
+
+  def file_stream(filename) do
+    File.stream!(filename, [:read], :line)
+    |> Stream.map(fn line -> line |> String.split(" ") |> Enum.count() end)
+    |> Enum.to_list()
+
+    # Returns a list of the number of words per line
+    # [33, 36, 33, 42, 21, 16, 12, 20, 19, 16, 10, 33, 36, 32, 38, 36, 39, 38, 15, 30,
+    #  18, 26, 22, 21, 40, 12, 27, 19, 15, 30, 21, 26, 37, 38, 29, 21, 15, 21, 25, 23,
+    #  21, 40, 28, 26, 44, 15, 22, 34, 18, 34, ...]
+  end
+
+  # CodeFlow.Streams.steps_in_enum2([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+  # CodeFlow.Streams.steps_in_stream2([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+  # TODO: create a graphic that explains/shows the Enum process and one for the Stream process
+  # The Stream doesn't execute a single call until something requests a value. Enum functions will do that.
 
   # CodeFlow.Streams.steps_in_stream3(1..10_000_000)
+
 
 
   @doc """
@@ -135,3 +201,49 @@ end
 # `parties` is how old-school Data Architects referred to that sort of thing
 # a `party` represents some individual or group that can engage in some other relationship
 # I learned it from a co-worker when I was at a bank. Here's a reasonable result from googling "data architecture pattern party" http://tdan.com/a-universal-person-and-organization-data-model/5014
+
+# TODO: Generate a CSV file using Faker names, phone numers, emails, etc. Create a value for 6 months of sales Jan-June. Stream process to get a total for June numbers
+
+# TODO: CodeDesign course -- use mix to create a project.
+#   - add mix dependencies
+#   - could monitor for CSV files to appear in a watched folder
+#   - use a GenServer to monitor... launch a Task to process a file
+#   - Generate a report file
+#   - drop multiple files in and have them concurrently processed
+#   - Can build all the pieces, put it together to have a working app
+#   - A specific file could intentionally cause it to blow up. Discuss supervision strategies?
+
+# Spotting problems:
+
+# Installing Erlang via `asdf` could show this...
+
+# Building Erlang/OTP 19.3.6.9 (asdf_19.3.6.9), please wait...
+# APPLICATIONS DISABLED (See: /home/mark/.asdf/plugins/erlang/kerl-home/builds/asdf_19.3.6.9/otp_build_19.3.6.9.log)
+#  * crypto         : No usable OpenSSL found
+#  * jinterface     : No Java compiler found
+#  * ssh            : No usable OpenSSL found
+#  * ssl            : No usable OpenSSL found
+
+# mark@mark-desktop-wd code_flow [master !?]$ asdf current
+# elixir         1.9.1-otp-22 (set by /home/mark/.tool-versions)
+# erlang         19.3.6.9 (set by /home/mark/thinking_code/code_flow/.tool-versions)
+
+# $ elixir -v
+# {"init terminating in do_boot",{undef,[{elixir,start_cli,[],[]},{init,start_em,1,[]},{init,do_boot,3,[]}]}}
+# init terminating in do_boot ()
+
+# Crash dump is being written to: erl_crash.dump...done
+
+# $ erl
+# Erlang/OTP 19 [erts-8.3.5.4] [source] [64-bit] [smp:4:4] [async-threads:10] [hipe] [kernel-poll:false]
+
+# Having an umbrella project, openning and editing an app within the umbrella. Need to open the root of the project.
+
+# Starting VS Code, the ElixirLS output shows the environment:
+
+#     Started ElixirLS
+#     Elixir version: "1.7.4 (compiled with Erlang/OTP 21)"
+#     Erlang version: "22"
+#
+# Note it shows OTP 21 yet the Erlang version is 22? They should be the same
+
